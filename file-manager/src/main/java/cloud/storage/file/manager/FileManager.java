ec14762173b24315b96b29c63a2078a144f8556d
Marlesss <334874@niuitmo.ru>
2023-11-02 18:26:16 +0300

    signing in limit added

--- file-manager/src/main/java/cloud/storage/file/manager/FileManager.java
+++ file-manager/src/main/java/cloud/storage/file/manager/FileManager.java
@@ -4,6 +4,8 @@ import cloud.storage.nio.UserData;
 import cloud.storage.util.Pair;
 
 import java.net.SocketAddress;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -12,13 +14,29 @@ import java.util.Objects;
  * Class responsible for managing inner file system and giving access to users.
  */
 public class FileManager {
+    private final static Duration PENALTY = Duration.ofMinutes(5);
+    private final static Integer SIGN_IN_ATTEMPTS_LIMIT = 3;
     private final Map<String, String> users;
     private final Map<String, SocketAddress> sessionByUser = new HashMap<>();
     private final Map<SocketAddress, String> userBySession = new HashMap<>();
+    private final Map<SocketAddress, Instant> addressPenalty = new HashMap<>();
+    private final Map<SocketAddress, Integer> addressSignInAttempts = new HashMap<>();
 
 
     public FileManager() {
         users = new HashMap<>();
+    }
+
+    private boolean addressIsBanned(SocketAddress address) {
+        if (!addressPenalty.containsKey(address)) {
+            return false;
+        }
+        Duration timePassed = Duration.between(addressPenalty.get(address), Instant.now());
+        if (timePassed.minus(PENALTY).isPositive()) {
+            addressPenalty.remove(address);
+            return false;
+        }
+        return true;
     }
 
     private Pair<Boolean, String> startSession(SocketAddress address, String login) {
@@ -63,6 +81,16 @@ public class FileManager {
     }
 
     public Pair<Boolean, String> signIn(SocketAddress address, UserData userData) {
+        if (addressIsBanned(address)) {
+            return new Pair<>(false, "Address is still banned. Please try again later.");
+        }
+        addressSignInAttempts.put(address, addressSignInAttempts.getOrDefault(address, 0) + 1);
+        if (addressSignInAttempts.get(address) > SIGN_IN_ATTEMPTS_LIMIT) {
+            addressSignInAttempts.remove(address);
+            addressPenalty.put(address, Instant.now());
+            return new Pair<>(false, "Address is banned due to excessive number of signing in attempts.\n" +
+                    "Please wait " + PENALTY.toMinutes() + " minutes and try again.");
+        }
         if (userBySession.containsKey(address)) {
             return new Pair<>(false, "There is an active session with such address already.");
         }
